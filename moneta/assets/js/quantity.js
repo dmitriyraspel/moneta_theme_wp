b202e20fb7b846ee72c98794bc189e1261211a50
dmitriyraspel@yandex.ru <2905Tati>
2021-08-19 13:10:48 +0300

    change quantity.js

--- moneta/assets/js/quantity.js
+++ moneta/assets/js/quantity.js
@@ -1,13 +1,21 @@
 function quantityBtn() {
-    var quantityBox = document.getElementsByClassName( 'quantity' );
+    var quantityBox = document.getElementsByClassName( 'quantity' ).getElementsByClassName( 'input-text' );
+    var btnDown = document.querySelectorAll('moneta-quantity-down');
+    var btnUp = document.querySelectorAll('moneta-quantity-up');
 
     if ( quantityBox ) {
-		console.log('quantityBtn');
+		console.log('quantityBtn' . quantityBox);
 	}
 
-    var newBtnUp = document.createElement("div");
-    newBtnUp.innerHTML = "+";
-    document.body.insertBefore(newBtnUp, quantityBox);
+    var value = quantityBox.value;
+    if ( value ) {
+		console.log('value' . value);
+	}
+    
+
+    // var newBtnUp = document.createElement("div");
+    // newBtnUp.innerHTML = "+";
+    // document.body.insertBefore(newBtnUp, quantityBox);
 
 };
 quantityBtn();
